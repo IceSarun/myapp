f6294a2933d1e0fc08a7e01e1130727395a2f231
IceSarun <sarunpawat.p@ku.th>
2023-07-08 13:21:34 +0700

    initial commit : add Cat

---
+++ src/myapp/Cat.java
@@ -0,0 +1,14 @@
+package myapp;
+
+public class Cat {
+    private String nameCat;
+    private int age;
+
+    public String getNameCat() {
+        return nameCat;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
